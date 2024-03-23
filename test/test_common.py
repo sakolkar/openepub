55eba2e0f430037e6d1102fea67cb15af63612af
Satyen Akolkar <9434751+sakolkar@users.noreply.github.com>
2024-03-23 14:47:36 -0600

    GH-2: add unit test for issue. Spaces stripped on spans in p tags

---
+++ test/test_common.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+import bs4
+
+from openepub.common import collect_text
+
+
+class CollectTextTestCase(TestCase):
+    def test_multiple_spans_in_p(self):
+
+        source = (
+            """<p class="Clanek1"><span lang="CS">Odpovědi na tyto a mnohé """
+            """další otázky hledá v novém senzačním životopise nazvaném </s"""
+            """pan><span lang="CS" class="calibre8">Život a lži Albuse Brum"""
+            """bála</span><span lang="CS"> Rita Holoubková. Exkluzivní rozh"""
+            """ovor s ní přináší Betty Braithwaiteová na straně 13.</span><"""
+            """/p>"""
+        )
+
+        expected = (
+            """Odpovědi na tyto a mnohé další otázky hledá v novém senzační"""
+            """m životopise nazvaném Život a lži Albuse Brumbála Rita Holou"""
+            """bková. Exkluzivní rozhovor s ní přináší Betty Braithwaiteová"""
+            """ na straně 13.\n"""
+        )
+
+        soup = bs4.BeautifulSoup(source.encode("utf-8"), "html.parser")
+        collected = collect_text(soup)
+
+        self.assertEqual(collected, expected)
